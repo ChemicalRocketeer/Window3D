b93ae2790f96e3d9813ab96a90493d444dfd05ff
David Aaron Suddjian <aasuddjian@gmail.com>
2015-10-13 18:12:56 -0600

    Initial project
    Project currently includes a scene with some shapes and a camera with a
    GyroController controlling its rotation using the device gyroscope.

---
+++ 3D Window/Assets/GyroStatusDisplay.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GyroStatusDisplay : MonoBehaviour {
+
+	
+	public Rect GUIArea = new Rect (10, 10, 500, 500);
+	
+	
+	/**
+	void Start () {
+		Input.gyro.enabled = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("updating rotation from gyro");
+		Gyroscope gyro = Input.gyro;
+		transform.rotation = gyro.attitude;
+	}
+
+**/
+	
+	void OnGUI () {
+		Debug.Log ("on gui");
+		GUIStyle mystyle = new GUIStyle (GUI.skin.label);
+		mystyle.fontSize = mystyle.fontSize * 4;
+		GUI.BeginGroup (GUIArea);
+		GUI.Label (new Rect (0, 0, 500, 50), "Rotation x: " + Input.gyro.attitude.eulerAngles.x, mystyle);
+		GUI.Label (new Rect (0, 50, 500, 50), "Rotation y: " + Input.gyro.attitude.eulerAngles.y, mystyle);
+		GUI.Label (new Rect (0, 100, 500, 50), "Rotation z: " + Input.gyro.attitude.eulerAngles.z, mystyle);
+		GUI.Label (new Rect (0, 150, 500, 50), SystemInfo.supportsGyroscope ? "Gyroscope supported" : "Gyrocope not supported!!", mystyle);
+		GUI.EndGroup ();
+	}
+}
